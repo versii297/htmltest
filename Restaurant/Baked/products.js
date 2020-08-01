550d9050db8909729125ad68f89f1e5eaec68329
versii297 <66777446+versii297@users.noreply.github.com>
2020-08-01 16:56:32 +0530

    baked update

--- Restaurant/Baked/products.js
+++ Restaurant/Baked/products.js
@@ -91,3 +91,199 @@ iceCreamBtn.classList.toggle('active');
 /* ------------end of ice cream menu ---------------------*/
 
 /* ----------------cakes menu-----------------------*/
+const cakeMenu = [
+  {
+   "item": "Chocolate Cake",
+   "price": " $70 ",
+   "image": "https://i.imgur.com/LyxAK7G.jpg"
+ },
+ {
+   "item": "Red Velvet Cake",
+   "price": " $80 ",
+   "image": "https://i.imgur.com/O7kOyWZ.jpg"
+ },
+ {
+   "item": "Fruit Cake",
+   "price": " $70 ",
+   "image": "https://i.imgur.com/EPk3jVH.jpg"
+ },
+ {
+   "item": "Oreo Cake",
+   "price": " $80 ",
+   "image": "https://i.imgur.com/Zqaz8l4.jpg"
+ },
+ {
+   "item": "Lemon Cream Cake",
+   "price": " $85 ",
+   "image": "https://i.imgur.com/hk4j8Iv.jpg"
+ },
+ {
+   "item": "RoseWater Raspberry Cake",
+   "price": " $85 ",
+   "image": "https://i.imgur.com/CMtxF6g.jpg"
+ },
+ {
+   "item": "Strawberry Cake",
+   "price": " $85 ",
+   "image": "https://i.imgur.com/CMtxF6g.jpg"
+ }
+];
+
+const cakeBtn = document.querySelector('#cakes');
+
+cakeBtn.addEventListener("click", () => {
+
+document.getElementById("menu-container").innerHTML = 
+`
+${cakeMenu.map(displayProduct).join('')}
+`;
+cakeBtn.classList.toggle('active');
+});
+
+
+/* ------------------- Brownies Menu ----------------- */
+const browniesMenu = [
+   {
+   "item": "Chocolate Brownies",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/YRRxGCv.jpg"
+ },
+ {
+   "item": "White Chocolate Brownies",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/VgXOYwC.jpg"
+ },
+ {
+   "item": "Chocolate Marshmallo Brownies",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/UAhiM9X.jpg"
+ },
+ {
+   "item": "Walnut Brownies",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/5BZHMeq.jpg"
+ },
+ {
+   "item": "White Chocolate Cranberry Fudge",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/AxYWvlz.jpg"
+ }
+];
+
+const brownieBtn = document.querySelector('#brownies');
+
+brownieBtn.addEventListener("click", () => {
+
+document.getElementById("menu-container").innerHTML = 
+`
+${browniesMenu.map(displayProduct).join('')}
+`;
+brownieBtn.classList.toggle('active');
+});
+
+
+/* ---------------- Parfaits Menu ------------ */
+const parfaitsMenu = [
+  {
+   "item": "Chocolate Parfait",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/cTM0Q2d.jpg"
+ },
+ {
+   "item": "Cheesecake Oreo Parfait",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/lGgdghu.jpg"
+ },
+ {
+   "item": "Mango Chocolate Parfait",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/HPaMvBC.jpg"
+ },
+ {
+   "item": "Strawberry Parfait",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/eip6aYO.jpg"
+ },
+ {
+   "item": "Yogurt Museli Parfait",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/qSDfZ9P.jpg"
+ },
+ {
+   "item": "Raspberry Chocolate Parfait",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/uVgNd1q.jpg"
+ }
+];
+
+const parfaitBtn = document.querySelector('#parfaits');
+
+parfaitBtn.addEventListener("click", () => {
+
+document.getElementById("menu-container").innerHTML = 
+`
+${parfaitsMenu.map(displayProduct).join('')}
+`;
+parfaitBtn.classList.toggle('active');
+});
+
+
+/* ----------------- Pudding Menu ------------------ */
+const puddingMenu = [
+  {
+   "item": "Custard Pudding",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/ex102HO.jpg"
+ },
+ {
+   "item": "Banana Cream Pudding",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/eW5d6wT.jpg"
+ },
+ {
+   "item": "Cheesecake Pudding",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/nrsp0lT.jpg"
+ },
+ {
+   "item": "Flaxseed Raspberry Pudding",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/XKXFQqZ.jpg"
+ },
+ {
+   "item": "Mango Pudding",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/ajLQYif.jpg"
+ },
+ {
+   "item": "Strawberry Chia Pudding",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/9a1baI0.jpg"
+ },
+ {
+   "item": "Lemon Pudding",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/wvNjfjf.jpg"
+ },
+ {
+   "item": "Tangerine Pudding",
+   "price": " $25 ",
+   "image": "https://i.imgur.com/m0mTeZv.jpg"
+ },
+ {
+   "item": "Vanilla & Chocolate Pudding",
+   "price": " $20 ",
+   "image": "https://i.imgur.com/JbYh8JH.jpg"
+ }
+]
+
+const puddingBtn = document.querySelector('#pudding');
+
+puddingBtn.addEventListener("click", () => {
+
+document.getElementById("menu-container").innerHTML = 
+`
+${puddingMenu.map(displayProduct).join('')}
+`;
+puddingBtn.classList.toggle('active');
+});
