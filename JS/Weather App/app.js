ff76342f720a8db376f39177d44128dc4fcfabde
versii297 <66777446+versii297@users.noreply.github.com>
2020-08-01 14:46:41 +0530

    c

--- JS/Weather App/app.js
+++ JS/Weather App/app.js
@@ -6,7 +6,7 @@ window.addEventListener('load', ()=> {
   let locationTimezone = document.querySelector('.location-timezone');
   let icon = document.querySelector('.icon');
   const temperatureSection = document.querySelector('.degree-section');
-const tempUnit = document.querySelector('.unit');
+  const tempUnit = document.querySelector('.unit');
 
 
 
@@ -14,6 +14,7 @@ const tempUnit = document.querySelector('.unit');
     navigator.geolocation.getCurrentPosition(position =>{
       long = position.coords.longitude;
       lat = position.coords.latitude;
+      
 
       
       const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&units=metric&appid=0fb2c3c779924b6da6ce1deb3efd9eab`;
@@ -24,7 +25,7 @@ const tempUnit = document.querySelector('.unit');
       })
       .then(data => {  
         // set DOM elements from API
-        tempDegree.textContent = `${data.current.temp}°`;
+        tempDegree.textContent = `${data.current.temp}° C`;
         tempDescription.textContent = data.current.weather[0].description;
         locationTimezone.textContent = data.timezone;
         icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png" alt="icon">`;
